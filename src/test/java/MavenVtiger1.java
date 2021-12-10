0c2605676dfa9bc0316c1165027ec4b98cc0f09a
Navee <Navee@Cartoon>
2021-12-10 10:02:18 +0530

    Class

---
+++ src/test/java/MavenVtiger1.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.Test;
+
+public class MavenVtiger1 {
+	
+	public class Testng1 {
+		@Test
+		
+		public void test() 
+		{
+			System.out.println("hello tyys");
+			
+			
+			
+		}
+	}
+
+}
